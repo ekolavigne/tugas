aa49bdf777905465b383fe22dff9f86c0f56b51f
ekolavigne <144085760+ekolavigne@users.noreply.github.com>
2023-09-14 21:59:33 +0700

    Create script.js

---
+++ tugas/css/script.js
@@ -0,0 +1,6 @@
+function hitungLuas(params) {
+    sisi = document.getElementById("txtSisi"). value;
+    sisi = document.getElementById("txtSisi").value;
+    Luas = sisi * sisi;
+    document.getElementById("txtLuas").value = Luas;
+}
